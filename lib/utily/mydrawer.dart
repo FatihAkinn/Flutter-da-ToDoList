6d9b0747644b1917224ac7b0379f808e0dd570ed
Fatih <127442816+FatihAkinn@users.noreply.github.com>
2023-06-05 23:40:30 +0300

    Add files via upload

---
+++ lib/utily/mydrawer.dart
@@ -0,0 +1,30 @@
+/* import 'package:flutter/material.dart';
+class Mydraver extends StatefulWidget {
+  const Mydraver({super.key});
+
+  @override
+  State<Mydraver> createState() => _MydraverState();
+}
+
+class _MydraverState extends State<Mydraver> {
+  @override
+  Widget build(BuildContext context) {
+    return const Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                image: const AssetImage("assets/images/efe.jpg")
+              ),
+            )],
+          ))
+        ],
+      ),
+    );
+  }
+} */
